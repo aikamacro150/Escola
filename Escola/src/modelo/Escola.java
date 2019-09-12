ec9206da93fe80e067fabcbe476ec79c682d36e7
Administrador <Administrador@10.10.4.6>
2019-09-12 16:30:57 -0300

    hoquebá

---
+++ Escola/src/modelo/Escola.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Escola {
+     private int NrAlunos;
+private int codigo;
+ private String nome;
+ private String endereco;
+ private String sigla;
+ private double area;
+
+    
+}
